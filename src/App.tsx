3bcd45b2be715b68d43a27c5405eb3750995888d
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-09-25 14:43:58 +0200

    Add MainLayout and LandingPage to Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import LandingPage from "./pages/LandingPage.tsx";
+import MainLayout from "./layouts/MainLayout.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LandingPage />} />
+      <Route element={<MainLayout />}>
+        {/* Landing page */}
+        <Route path="/" element={<LandingPage />} />
+      </Route>
+
       {/* register page */}
       <Route path="/register" element={<RegisterPage />} />
       {/* fallback */}
