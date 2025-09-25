11529cd858807ecff4e1b4bd8db20667f693abae
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-09-25 14:44:30 +0200

    Add Hero component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import "./Hero.css";
+import { Link } from "react-router-dom";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-overlay">
+        <h1>
+          BUILD <span className="highlight">YOUR</span> BODY <span className="highlight">STRONG</span>
+        </h1>
+        <h2>Ready to change your physique?</h2>
+
+        <div className="hero-links">
+          <p>Join us today</p>
+          <p className="hero-auth-links">
+            <Link to="/register" className="">
+              Signup
+            </Link>{" "}
+            or <Link to="/">Login</Link>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
