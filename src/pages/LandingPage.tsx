5b4cc86612ca66b2b99f363e1171835b109cefe6
Felicia Dahl <feliciadahl01@gmail.com>
2025-09-26 14:43:10 +0200

    Add Nav to Header and Header in MainLayout

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,11 @@
 import Hero from "../components/Hero";
-import Nav from "../components/Nav";
+
 
 const LandingPage = () => {
   return (
     <div>
       <Hero />
-      <Nav />
+    
     </div>
   );
 };
