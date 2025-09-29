7f82394171c90b00b710a8ec53277ebeb52cb6c3
ugudmundsson <u.gudmundsson@gmail.com>
2025-09-29 12:45:39 +0200

    fetch for data to workout cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,11 @@
 import "./Card.css";
+import type { Workout } from "../components/WorkoutList/WorkoutList";
 
-const HoverCard = () => {
+interface CardProps {
+  dataObj: Workout;
+}
+
+const Card: React.FC<CardProps> = ({ dataObj }) => {
   return (
     <div className="card">
       <div className="image-section">
@@ -12,9 +17,7 @@ const HoverCard = () => {
 
         {/* Overlay schema (vänster) */}
         <div className="schedule">
-          <div className="day">
-            THU <span>13:30</span>
-          </div>
+          <div className="day">{dataObj.date}</div>
           <div className="day">
             FRI <span>17:00</span>
           </div>
@@ -26,24 +29,30 @@ const HoverCard = () => {
         {/* Overlay info (höger) */}
         <div className="info-box">
           <p>
-            <i className="fa-solid fa-users"></i> Platser: 25
+            <i className="fa-solid fa-users"></i> Platser:<br></br>{" "}
+            {dataObj.capacity}
           </p>
           <p>
-            <i className="fa-solid fa-location-dot"></i> Göteborg
+            <i className="fa-solid fa-location-dot"></i> Plats:<br></br>{" "}
+            {dataObj.location}
+          </p>
+          <p>
+            <i className="fa-solid fa-location-dot"></i>Info:<br></br>{" "}
+            {dataObj.details}
           </p>
         </div>
       </div>
 
       {/* Innehåll */}
       <div className="content-section">
-        <h2 className="title">Yoga</h2>
+        <h2 className="title">{dataObj.title}</h2>
         <div className="participants">
           <i className="fa-solid fa-user"></i>
-          <span className="participants-text">Greta Thunberg</span>
+          <span className="participants-text">{dataObj.instructor}</span>
         </div>
       </div>
     </div>
   );
 };
 
-export default HoverCard;
+export default Card;
--- src/components/WorkoutList/WorkoutList.tsx
+++ src/components/WorkoutList/WorkoutList.tsx
@@ -1,17 +1,43 @@
 import "./WorkoutList.css";
 import Card from "../Card";
 import "../WorkoutList/WorkoutList.css";
+import { useState, useEffect } from "react";
 
-const WorkoutList = () => {
+export interface Workout {
+  id: string;
+  title: string;
+  date: string;
+  location: string;
+  instructor: string;
+  details: string;
+  capacity: string;
+}
+
+const WorkoutList: React.FC = () => {
+  const [data, setData] = useState<Workout[]>([]);
+
+  const url =
+    "https://grp10workoutserviceapp.azurewebsites.net/api/Workout/getall";
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="container-workoutlist">
         <div className="workout-list">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {data.map((dataObj) => (
+            <Card key={dataObj.id} dataObj={dataObj} />
+          ))}
         </div>
       </div>
     </>
