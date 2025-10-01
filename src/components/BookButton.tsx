c837d2b10083c0bbab7cd9c253e684a3e219fe72
Felicia Dahl <feliciadahl01@gmail.com>
2025-10-01 15:30:36 +0200

    Add BookinButton

---
+++ src/components/BookButton.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+interface ButtonProps {
+    workoutId: string;
+    userId: string;
+}
+
+const BookButton : React.FC<ButtonProps>= ({ workoutId, userId} ) => {
+    const handleBooking = async () => {
+
+        try {
+            const res = await fetch("https://grp10bookingapp.azurewebsites.net/api/Booking/create", {
+                method: "POST",
+                headers: {"Content-Type": "application/json",},
+                body: JSON.stringify({
+                    workoutId,
+                    userId,
+                    quantity:1,
+                }),
+    
+            })
+
+
+            if(res.ok) {
+            
+                //Add Confimation//
+                console.log("Booking created")
+                console.log(res)
+
+            }
+            else {
+                const error = await res.text();
+                alert(`Something went wrong: ${error}`);
+            }
+    }
+    catch(error) {
+         console.error("Error booking workout:", error);
+    }
+}
+  return (
+    <button className='"book-btn' onClick={handleBooking}>Book Here</button>
+  )
+}
+
+export default BookButton
