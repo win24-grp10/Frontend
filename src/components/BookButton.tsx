00b0715cf7facb244402add7a23e2997614cc7f0
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-10-03 10:50:12 +0200

    Add callback to BookButton

--- src/components/BookButton.tsx
+++ src/components/BookButton.tsx
@@ -4,37 +4,34 @@ interface ButtonProps {
   workoutId: string;
   userId: string;
   onRefresh?: () => void;
+  onConfirmation: (message: string, type: "success" | "error") => void;
 }
 
-const BookButton: React.FC<ButtonProps> = ({
-  workoutId,
-  userId,
-  onRefresh,
-}) => {
+const BookButton: React.FC<ButtonProps> = ({ workoutId, userId, onRefresh, onConfirmation }) => {
   const handleBooking = async () => {
     try {
-      const res = await fetch(
-        "https://grp10bookingapp.azurewebsites.net/api/Booking/create",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            workoutId,
-            userId,
-            quantity: 1,
-          }),
-        }
-      );
+      const res = await fetch("https://grp10bookingapp.azurewebsites.net/api/Booking/create", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          workoutId,
+          userId,
+          quantity: 1,
+        }),
+      });
 
       if (res.ok) {
         //Add Confimation//
         onRefresh && onRefresh();
+
+        onConfirmation("Bokning genomförd!", "success");
       } else {
         const error = await res.text();
-        alert(`Something went wrong: ${error}`);
+        onConfirmation(`Bokning misslyckades: ${error}`, "error");
       }
     } catch (error) {
       console.error("Error booking workout:", error);
+      onConfirmation("Nätverksfel, försök senare.", "error");
     }
   };
   return (
